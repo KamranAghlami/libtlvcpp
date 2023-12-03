fa7fdc8f3d829bef6dbe414441fd132fee643980
Kamran <oops.legendary@gmail.com>
2023-12-03 11:03:35 +0300

    .

--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,29 @@ namespace ka
 {
     template <>
     template <>
+    tlv_tree::tree(const tlv_tree &other) : life_time(other), m_root(other.root())
+    {
+        std::cout << "const\n";
+    }
+
+    template <>
+    template <>
     tlv_tree::tree(tlv_tree &other) : life_time(other), m_root(other.root())
     {
+        std::cout << "non-const\n";
     }
 }
 
 int main()
 {
-    ka::tlv_tree tlvt{};
+    const ka::tlv_tree tlvt{};
 
-    auto &l0 = tlvt.root();
-    auto &l11 = l0.add_node(1U, "Hello, World!");
-    auto &l12 = l0.add_node(2U, "0123456789abcdefABCDEF");
+    // auto &l0 = tlvt.root();
+    // auto &l11 = l0.add_node(1U, "Hello, World!");
+    // auto &l12 = l0.add_node(2U, "0123456789abcdefABCDEF");
 
-    l11.add_node(3U, "Hello, World!!!!");
-    l12.add_node(4U, "A not so long sentence!");
+    // l11.add_node(3U, "Hello, World!!!!");
+    // l12.add_node(4U, "A not so long sentence!");
 
     ka::tlv_tree tlvt2{tlvt};
 
