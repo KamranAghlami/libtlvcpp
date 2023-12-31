1dbd37d2fe0de7dd11361103e5062b9b0b057742
Kamran <oops.legendary@gmail.com>
2023-12-31 14:42:11 +0300

    narrowed down the serialization bug

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,19 @@ int main()
 {
     ka::tlv_tree_node tlvt(0b00100000U | 0x00U, "This is the root node.");
 
-    auto &n1 = tlvt.add_child(0b00100000U | 0x01U);
-    tlvt.add_child(0x02U, "There's a pattern as you see...");
+    // auto &n1 = tlvt.add_child(0b00100000U | 0x01U);
+    // tlvt.add_child(0x02U, "There's a pattern as you see...");
     auto &n3 = tlvt.add_child(0b00100000U | 0x03U);
 
-    n1.add_child(0x04U, "According to the tlv standard...");
-    n1.add_child(0x05U, "Nodes that have children...");
+    // n1.add_child(0x04U, "According to the tlv standard...");
+    // n1.add_child(0x05U, "Nodes that have children...");
 
     n3.add_child(0x06U, "They should have 6'th bit set in the highest byte.");
     auto &n7 = n3.add_child(0b00100000U | 0x07U);
 
     n7.add_child(0x08U, "The serializer and deserializer depend on this.");
     n7.add_child(0x09U, "So they either have some data or children, not both.");
-    n7.add_child(0x0AU, "That's why the root node's data is missing in the serialized/deserialized output.");
+    // n7.add_child(0x0AU, "That's why the root node's data is missing in the serialized/deserialized output.");
 
     std::cout << "tree:\n";
     tlvt.dump();
